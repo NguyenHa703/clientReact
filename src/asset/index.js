90abccd48c18f2bd2c20a5cf0db40a9b9f4d018a
Nguyễn Thị Thu Hà <thuha171003@gmail.com>
2023-12-25 22:25:26 +0700

    first commit

---
+++ src/asset/index.js
@@ -0,0 +1,8 @@
+import logo from "./logo.png"
+import phone from "./phone.png"
+import facebook from "./facebook.png"
+import cart from "./cart.png"
+import google from "./google.png"
+
+
+export {logo, phone, facebook, cart, google}
---
+++ src/components/Header.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import { cart, logo } from '../asset/index'
+
+const Header = () => {
+    return (
+        <div className='w-full h-20 bg-white border-b-[1px] border-b-gray-800'>
+            <div className='max-w-screen-xl h-full mx-auto flex items-center justify-between'>
+                <div className='w-28'>
+                    <img src={logo} alt='logo' />
+                </div>
+                <div></div>
+                <div className='flex items-center gap-8'>
+                    <ul className='flex items-center gap-8'>
+                        <li>Home</li>
+                        <li>Pages</li>
+                        <li>Shop</li>
+                        <li>Element</li>
+                        <li>Blog</li>
+                    </ul>
+                    <div className='relative'>
+                        <img  className='w-7' src = {cart}  alt='cart'/>
+                        <span className='absolute w-7 top-2 bottom-1 left-0 text-sm flex items-center justify-center font-semibold font-titleFont'>0</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Header
+
